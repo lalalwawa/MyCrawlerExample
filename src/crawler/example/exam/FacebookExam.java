77d8d7b810bd316087741af8172b090ccc3a2271
Alicia Lee <poiupoiu.alicia@gmail.com>
2017-12-10 11:30:07 +0800

    page爬讚

--- src/crawler/example/exam/FacebookExam.java
+++ src/crawler/example/exam/FacebookExam.java
@@ -25,26 +25,36 @@ public class FacebookExam {
 
 		String uri = 
 				"https://graph.facebook.com/v2.6"
-				+ "/search?q="
-				+ "&access_token=";
+				+ "/professorofpower/posts?fields=id,link,message,created_time,likes.limit(0).summary(total_count),reactions.limit(0).summary(total_count)"
+				+ "&access_token=EAACEdEose0cBAGiYHbuv3MqjidZComnmJELOVOpv1wcikPPL3FUCqaSfg87SKmeSCIBZCaGO9BunhMzXAWuYRnzV8eSY2tqN9EdgPbxgWz8uUrxh2ZBbzqNFL4R2XZCzJigLeyATdwdAJRXfI6YQkuxlzBgn5EgGZAelMSgt12OSxRoR7ZAQkfDN23A3CIBuAZD";
 
+		String uri2 =
+				"https://graph.facebook.com/v2.6"
+						+ "/professorofpower/posts?fields=id,link,message,created_time,likes.limit(0).summary(total_count),reactions.type(HAHA).limit(0).summary(total_count)"
+						+ "&access_token=EAACEdEose0cBAGiYHbuv3MqjidZComnmJELOVOpv1wcikPPL3FUCqaSfg87SKmeSCIBZCaGO9BunhMzXAWuYRnzV8eSY2tqN9EdgPbxgWz8uUrxh2ZBbzqNFL4R2XZCzJigLeyATdwdAJRXfI6YQkuxlzBgn5EgGZAelMSgt12OSxRoR7ZAQkfDN23A3CIBuAZD";
 
 		Elements elems =
 				CrawlerPack.start()
 				.getFromJson(uri)
 				.select("data");
-		
-		String output = "id,reactions";
+
+		Elements elems2 =
+				CrawlerPack.start()
+						.getFromJson(uri)
+						.select("data");
+
+		String output = "id, total_reactions\n";
 
 		// 遂筆處理
 		for( Element data: elems ){
 			String id = data.select("id").text();
 
 			// FIXIT
-			String reactions = "";
+			String reactions = data.select("reactions summary total_count").text();
 
+			output += id + "," + reactions;
 
-			output += id + "," + reactions + "\n";
+			output += "\n";
 		}
 
 		System.out.println( output );
